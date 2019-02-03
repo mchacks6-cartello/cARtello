b40b87f9acd4bedd11c1c4d81a67f86760eb2156
Russell Pecka <rpecka@users.noreply.github.com>
2019-02-02 16:47:48 -0800

    Added support for a car model with injectable decision algorithms (#3)

---
+++ CarModel/CarModel.h
@@ -0,0 +1,19 @@
+//
+//  CarModel.h
+//  CarModel
+//
+//  Created by Russell Pecka on 2/2/19.
+//  Copyright © 2019 Russell Pecka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarModel.
+FOUNDATION_EXPORT double CarModelVersionNumber;
+
+//! Project version string for CarModel.
+FOUNDATION_EXPORT const unsigned char CarModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarModel/PublicHeader.h>
+
+
