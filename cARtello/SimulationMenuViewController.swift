0ad752284776b4c205c716595fbf04184b27f8fd
Gaurangi Siriya <s.gaurangi12@gmail.com>
2019-02-03 00:46:46 -0500

    Simulation menu 2

---
+++ cARtello/SimulationMenuViewController.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  cARtello
+//
+//  Created by Gaurangi Siriya on 2019-02-02.
+//  Copyright © 2019 Russell Pecka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SimulationMenuViewController: UIViewController {
+    
+    @IBOutlet var multiplierTextField: UITextField!
+    
+     @IBOutlet var
+    frequencyTextField: UITextField!
+    @IBOutlet var
+    button: UIButton!
+    
+    @IBOutlet var
+    pageCount: UIPageControl!
+    
+}
