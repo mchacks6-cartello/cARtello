a5fc102c5bb6f074af3119aaa50eb3705e9f47cd
Russell Pecka <rpecka@users.noreply.github.com>
2019-02-08 12:04:52 -0800

    Added remaining features for demo (#7)
    * Render a map tile
    
    * Resize map when rerender
    
    * Adding delegate for realTimeSim
    
    * Change floor to plane like example
    
    * Zoomies
    
    * Moving car wow
    
    * Made the map big
    
    * coordinates - not working yet
    
    * Text for network
    
    * Smooth operator
    
    * Fixed car size and algorithm
    
    * Removed unused files

--- CarModel/CarModel.swift
+++ CarModel/CarModel.swift
@@ -17,25 +17,32 @@ public class CarModel {
     
     public private(set) var connectionState: ConnectionState = .disconnected
     
+    public var currNetwork: String!
+    public var availableNetworks: [String] = []
+    
     private let decisionAlgorithm: DecisionAlgorithm
     
     public weak var delegate: CarModelDelegate?
     
     public init(decisionAlgorithm: DecisionAlgorithm) {
         self.decisionAlgorithm = decisionAlgorithm
+        self.currNetwork = "Not connected"
     }
     
     public func process(networks: [Network]) {
+        availableNetworks = networks.map { $0.name }
         switch decisionAlgorithm.networkChoice(for: connectionState, availableNetworks: networks) {
         case .stay:
             break
         case .fail:
             connectionState = .disconnected
             delegate?.carModel(self, didUpdateConnectionState: connectionState)
+            self.currNetwork = "Not connected"
         case .change(let newNetwork):
             connectionState = .connected(currentNetwork: newNetwork)
             delegate?.carModel(self, didUpdateConnectionState: connectionState)
+            self.currNetwork = newNetwork
+
         }
     }
-    
 }
--- cARtello/ViewController.swift
+++ cARtello/ViewController.swift
@@ -11,58 +11,151 @@ import SceneKit
 import ARKit
 import CoreLocation
 import MapKit
+import DataIngest
+import RealTimeSim
+import CarModel
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, RealTimeSimDelegate, CarModelDelegate {
+    
+    
+    enum Constants {
+        static let tileDimen: CGFloat = 1
+        static let initialRadius: Double = 0.005
+    }
+    
+    @IBOutlet weak var sessionInfoLabel: UILabel!
+    @IBOutlet weak var sessionInfoView: UIView!
     @IBOutlet var sceneView: ARSCNView!
     let config = ARWorldTrackingConfiguration()
-    var mapTile: UIImage!
+    let dataIngest: DataIngest! = DataIngest.init(jsonName: "ExperimentalData")
+    var realTimeSim: RealTimeSim!
+    let carModel = CarModel(decisionAlgorithm: PowerLevelAvoidDisconnectAlgorithm(minimumRSSI: -50))
+    
+    var anchorVector: simd_float3!
+
+    var node: SCNNode!
+    var tile: SCNNode!
+    var car: SCNNode!
+    var currentCarPosLat: Double!
+    var currentCarPosLong: Double!
+    var currentCarPosVector: SCNVector3!
+
+    
+    lazy var center = dataIngest.dataPoints.first!
+    
+    var currentRadius = Constants.initialRadius
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       // sceneView.debugOptions =
-            //[ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
-        config.planeDetection = .horizontal
-        
-        sceneView.session.run(config)
+        realTimeSim = RealTimeSim.init(dataIngest: dataIngest, frequency: 10)
+        carModel.delegate = self
+        realTimeSim.delegate = self
+    }
     
-        sceneView.delegate = self
+    /// - Tag: StartARSession
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        createMapTile(coordinate: CLLocationCoordinate2DMake(45.45958658333333, -73.82274576666667)) {(snapshot, err) in
-            guard let image = snapshot?.image else {
-                return
-            }
-            self.mapTile = image
+        // Start the view's AR session with a configuration that uses the rear camera,
+        // device position and orientation tracking, and plane detection.
+        config.planeDetection = [.horizontal]
+        config.worldAlignment = .gravityAndHeading
+        sceneView.session.run(config)
+        
+        // Set a delegate to track the number of plane anchors for providing UI feedback.
+        sceneView.session.delegate = self
+        
+        // Prevent the screen from being dimmed after a while as users will likely
+        // have long periods of interaction without touching the screen or buttons.
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // Show debug UI to view performance metrics (e.g. frames per second).
+        sceneView.showsStatistics = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's AR session.
+        sceneView.session.pause()
+    }
+    
+
+    func createCarNode(anchor: simd_float3) -> SCNNode {
+        let side: CGFloat = 0.01
+        let carNode = SCNNode(geometry: SCNBox(width: side, height: side, length: side, chamferRadius: 0.005))
+        carNode.position = SCNVector3(anchor.x, 0, anchor.z)
+        carNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red;
+        return carNode
+    }
+    
+    func createFloorNode(anchor: simd_float3, mapTile: UIImage) -> SCNNode {
+        let floorNode = SCNNode(geometry: SCNPlane(width: Constants.tileDimen, height: Constants.tileDimen))
+        floorNode.position = SCNVector3(anchor.x, 0, anchor.z)
+        floorNode.orientation = SCNQuaternion(0, 0, 0, 0)
+        floorNode.geometry?.firstMaterial?.diffuse.contents = mapTile
+        floorNode.geometry?.firstMaterial?.isDoubleSided = true
+        floorNode.eulerAngles = SCNVector3(Double.pi/2,0,-Double.pi)
+        return floorNode
+    }
+    
+    func setCarPos(_ dataPoint: DataPoint) {
+        if self.currentCarPosLat != nil {
+            self.currentCarPosVector = self.car.position
+            let newVec = scaleCoordinates(latitude: dataPoint.latitude, longitude: dataPoint.longitude)
+            SCNTransaction.animationDuration = 1.0
+            var vecAnchor = SCNVector3()
+            vecAnchor.x = self.tile.position.x - newVec.z
+            vecAnchor.z = newVec.x + self.tile.position.z
+            self.car.position = vecAnchor
+            self.currentCarPosLat = dataPoint.latitude
+            self.currentCarPosLong = dataPoint.longitude
+        } else {
+            let carNode = self.createCarNode(anchor: self.anchorVector)
+            self.node.addChildNode(carNode)
+            self.car = carNode
+            
+            self.currentCarPosLat = dataPoint.latitude
+            self.currentCarPosLong = dataPoint.longitude
         }
     }
     
-    func createFloorNode(anchor:ARPlaneAnchor) ->SCNNode{
-        let floorNode = SCNNode(geometry: SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))) //1
-        floorNode.position=SCNVector3(anchor.center.x,0,anchor.center.z)                                               //2
-        floorNode.geometry?.firstMaterial?.diffuse.contents = self.mapTile                                          //3
-        floorNode.geometry?.firstMaterial?.isDoubleSided = true                                                        //4
-        floorNode.eulerAngles = SCNVector3(Double.pi/2,0,0)                                                    //5
-        return floorNode                                                                                               //6
+    func scaleCoordinates(latitude: Double, longitude: Double)  -> SCNVector3 {
+        let diffLat = latitude - center.latitude
+        let diffLong = longitude - center.longitude
+        
+        let tileLat: CGFloat = CGFloat(dataIngest.metadata.maxLatitude - self.dataIngest.metadata.minLatitude)
+        let tileLong: CGFloat = CGFloat(dataIngest.metadata.maxLongitude - self.dataIngest.metadata.minLongitude)
+        
+        
+
+        return SCNVector3(CGFloat(diffLat) /  tileLat * (Constants.tileDimen / 2),0, CGFloat(diffLong) /  tileLong * (Constants.tileDimen / 2))
+    
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else {return} //1
+        guard self.anchorVector == nil else { return }
         
-        let planeNode = self.createFloorNode(anchor: planeAnchor)
-        node.addChildNode(planeNode)
+        // Place content only for anchors found by plane detection.
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        self.anchorVector = planeAnchor.center
+        self.node = node
+        createMapTile() {(snapshot, err) in
+            guard let image = snapshot?.image else {
+                print(err.debugDescription)
+                return
+            }
+            if let tile = self.tile {
+                tile.geometry?.firstMaterial?.diffuse.contents = image
+            } else {
+                let node = self.createFloorNode(anchor: self.anchorVector, mapTile: image)
+                self.node.addChildNode(node)
+                self.tile = node
+                self.realTimeSim.start()
+            }
+        }
     }
     
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
-        node.enumerateChildNodes { (node, _) in
-            node.removeFromParentNode()
-        }
-        
-        let planeNode = self.createFloorNode(anchor: planeAnchor)
-        node.addChildNode(planeNode)
-    }
-
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
         guard let _ = anchor as? ARPlaneAnchor else {return}
         node.enumerateChildNodes { (node, _) in
@@ -70,15 +163,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
-    func createMapTile(coordinate: CLLocationCoordinate2D, completion: @escaping MKMapSnapshotter.CompletionHandler) {
-        let radius: Double = 1000
+    func createMapTile(completion: @escaping MKMapSnapshotter.CompletionHandler) {
         let mapSnapshotOptions = MKMapSnapshotter.Options()
         
-        let location = coordinate
-        let region = MKCoordinateRegion(center: location, latitudinalMeters: radius, longitudinalMeters: radius)
+        let (latitude, longitude) = (center.latitude, center.longitude)
+        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        let metadata = dataIngest.metadata
+        let region = MKCoordinateRegion.init(center: location, span: .init(latitudeDelta: metadata.maxLatitude - metadata.minLatitude, longitudeDelta: metadata.maxLongitude - metadata.minLongitude))
         mapSnapshotOptions.region = region
         mapSnapshotOptions.scale = UIScreen.main.scale
-        mapSnapshotOptions.size = CGSize(width: 300, height: 300)
+        mapSnapshotOptions.size = CGSize(width: 1200, height: 1200)
         mapSnapshotOptions.showsBuildings = true
         mapSnapshotOptions.showsPointsOfInterest = true
         
@@ -86,32 +180,144 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         snapShotter.start(completionHandler: completion);
     }
     
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    // MARK: - RealTimeSimDelegate
+    func realTimeSim(_ realTimeSim: RealTimeSim, didPingDataPoint dataPoint: DataPoint) {
+//        if currentMaxLat == nil {
+//            setCurrentBoogles(with: dataPoint)
+//            setTileImage(dataPoint)
+//        } else if dataPoint.latitude > currentMaxLat || dataPoint.latitude < currentMinLat || dataPoint.longitude > currentMaxLong || dataPoint.longitude < currentMinLong {
+//            currentRadius += Constants.initialRadius
+//            setCurrentBoogles(with: dataPoint)
+//            setTileImage(dataPoint)
+//        }
+        
+        setCarPos(dataPoint)
+        self.carModel.process(networks: dataPoint.networks)
     }
-*/
     
-//     func session(_ session: ARSession, didFailWithError error: Error) {
-//         Present an error message to the user
+    
+    func setTileImage(_ dataPoint: DataPoint) {
+        createMapTile() {(snapshot, err) in
+            guard let image = snapshot?.image else {
+                print(err.debugDescription)
+                return
+            }
+            if let tile = self.tile {
+                tile.geometry?.firstMaterial?.diffuse.contents = image
+            } else {
+                let node = self.createFloorNode(anchor: self.anchorVector, mapTile: image)
+                self.node.addChildNode(node)
+                self.tile = node
+            }
+        }
+    }
+    
+    func realTimeSimDidFinishSimulation(_ realTimeSim: RealTimeSim) {
+        print("Done")
+    }
+    
+    // MARK: - ARSessionDelegate
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+//        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+//        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+//        updateSessionInfoLabel(for: session.currentFrame!, trackingState: camera.trackingState)
+    }
+    
+    // MARK: - ARSessionObserver
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay.
+        sessionInfoLabel.text = "Session was interrupted"
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required.
+        sessionInfoLabel.text = "Session interruption ended"
+        resetTracking()
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        sessionInfoLabel.text = "Session failed: \(error.localizedDescription)"
+        guard error is ARError else { return }
+        
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        
+        // Remove optional error messages.
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        
+        DispatchQueue.main.async {
+            // Present an alert informing about the error that has occurred.
+            let alertController = UIAlertController(title: "The AR session failed.", message: errorMessage, preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+                alertController.dismiss(animated: true, completion: nil)
+                self.resetTracking()
+            }
+            alertController.addAction(restartAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - Private methods
+    
+    private func updateSessionInfoLabel(for frame: ARFrame, trackingState: ARCamera.TrackingState) {
+        // Update the UI to provide feedback on the state of the AR experience.
+//        let message: String
+        
+//        switch trackingState {
+//        case .normal where frame.anchors.isEmpty:
+//            // No planes detected; provide instructions for this app's AR interactions.
+//            message = "Move the device around to detect horizontal and vertical surfaces."
 //
-//    }
+//        case .notAvailable:
+//            message = "Tracking unavailable."
 //
-//    func sessionWasInterrupted(_ session: ARSession) {
-//         Inform the user that the session has been interrupted, for example, by presenting an overlay
-//        
-//    }
+//        case .limited(.excessiveMotion):
+//            message = "Tracking limited - Move the device more slowly."
 //
-//    func sessionInterruptionEnded(_ session: ARSession) {
-//         Reset tracking and/or remove existing anchors if consistent tracking is required
-//        
-//    }
+//        case .limited(.insufficientFeatures):
+//            message = "Tracking limited - Point the device at an area with visible surface detail, or improve lighting conditions."
+//
+//        case .limited(.initializing):
+//            message = "Initializing AR session."
+//
+//        default:
+//            // No feedback needed when tracking is normal and planes are visible.
+//            // (Nor when in unreachable limited-tracking states.)
+//            message = ""
+//
+//        }
+////
+//        sessionInfoLabel.text = message
+//        sessionInfoView.isHidden = message.isEmpty
+    }
+    
+    private func resetTracking() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func carModel(_ carModel: CarModel, didUpdateConnectionState connectionState: ConnectionState) {
+        let string = "Possible networks: \n" +
+
+            carModel.availableNetworks.joined(separator: "\n") + "\n --- \nConnected to: " + carModel.currNetwork
+        self.sessionInfoLabel.text = string
+        
+    }
 }
 
 
